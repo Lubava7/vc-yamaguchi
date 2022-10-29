d1961a78de34f0a2d2f9f3cdf7a50f1e1460758b
Lubava7 <87942242+Lubava7@users.noreply.github.com>
2022-10-29 19:44:19 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const blackHole = document.querySelector(".black-hole");
+const bounceDoll = document.querySelector(".bounce");
+
+// if (window.innerWidth > 320) {
+//   // alert("hjhhh"); //работает
+// }
+
+// function dollAppear() {
+//   // alert("Привет"); //работает
+//   blackHole.style.height = "50px";
+//   blackHole.style.width = "120px";
+// }
+// setTimeout(dollAppear, 1000);
